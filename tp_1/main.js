3e9dfc9bd5e1ebef3656a4e6a260168e1c95485f
FedeSanguine <101578998+FedeSanguine@users.noreply.github.com>
2023-10-16 17:30:31 -0300

    .

--- tp_1/main.js
+++ tp_1/main.js
@@ -1,24 +1,21 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// En este archivo vamos a definir los servicios de Firebase que vamos a utilizar, y exportarlos para ser
+// consumidos por otros servicios.
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
 
-setupCounter(document.querySelector('#counter'))
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Obtener la instancia de Firestore.
+export const db = getFirestore(app);
+
